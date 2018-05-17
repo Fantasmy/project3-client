ac22fcbd8d4cdb6772715894b35f88ab7de57fd9
Fantasmy <fantazmit@gmail.com>
2018-05-17 12:08:33 +0200

    fixed event bar info address

--- src/app/components/bar-info-card/bar-info-card.component.ts
+++ src/app/components/bar-info-card/bar-info-card.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class BarInfoCardComponent implements OnInit {
 
   @Input() bar: Object;
+  @Input() location: Object;
 
   constructor() { }
 
